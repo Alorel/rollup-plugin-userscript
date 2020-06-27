d2dd48fd9c29e56f6c370fd879840f202cd425ed
Arturas Molcanovas <a.molcanovas@gmail.com>
2020-06-27 14:48:36 +0100

    feat: Add support for different build stages

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export {BuildStage} from './lib/BuildStage';
 export {UserscriptDefinition} from './lib/UserscriptDefinition';
 export {getPkgVersion} from './lib/get-pkg-version';
 export {UserscriptPluginOpts, userscriptPlugin} from './lib/plugin';
